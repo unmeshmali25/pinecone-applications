e285a7dcf8530a0e36baac50f78992dd9a0f1354
Unemsh <unmesh.mali@cvshealth.com>
2024-03-13 16:22:30 -0400

    Pinecode API and index
    Signed up for pinecode API and got the keys and created index

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pinecone-applications
 Follow along of Deeplearning.ai's short course
+------------------------------------------
+## Debug Logs
+HTTP response headers: HTTPHeaderDict({'content-type': 'text/plain; charset=utf-8', 'access-control-allow-origin': '*', 'vary': 'origin,access-control-request-method,access-control-request-headers', 'access-control-expose-headers': '*', 'X-Cloud-Trace-Context': 'ee54128888e1cde583668edbffc72c98', 'Date': 'Wed, 13 Mar 2024 20:16:18 GMT', 'Server': 'Google Frontend', 'Content-Length': '136', 'Via': '1.1 google', 'Alt-Svc': 'h3=":443"; ma=2592000,h3-29=":443"; ma=2592000'})
+HTTP response body: {"error":{"code":"FORBIDDEN","message":"Index creation failed. To create serverless indexes, you must upgrade your plan."},"status":403}
+------------------------------------------
--- semanticSearch.py
+++ semanticSearch.py
@@ -6,7 +6,55 @@ from sentence_transformers import SentenceTransformer
 from pinecone import Pinecone, ServerlessSpec
 from DLAIUtils import Utils
 import DLAIUtils
-
+import json
 import os
 import time
 import torch
+from tqdm.auto import tqdm
+
+
+dataset = load_dataset('quora', split='train[240000:290000]')
+print(f"dataset downloaded successfully..")
+
+questions = []
+for record in dataset['questions']:
+    questions.extend(record['text'])
+question = list(set(questions))
+print('\n'.join(questions[:10]))
+print('-' * 50)
+print(f'Number of questions: {len(questions)}')
+
+
+# Check CUDA and setup the model
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+if device != 'cuda':
+    print('Sorry no cuda.')
+model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
+
+# Query adn encoding using the all-MiniLM-L6-v2 model
+query = 'which city is the most populated in the world?'
+xq = model.encode(query)
+xq.shape
+
+# Steup Pinecone
+utils = Utils()
+
+with open('./pineapi.json', 'r') as file:
+	data = json.load(file)
+os.environ['PINECONE_API_KEY'] = data['PINECONE_API_KEY']
+PINECONE_API_KEY = data['PINECONE_API_KEY']
+
+
+pinecone = Pinecone(api_key=PINECONE_API_KEY)
+INDEX_NAME = utils.create_dlai_index_name('dl-ai')
+
+if INDEX_NAME in [index.name for index in pinecone.list_indexes()]:
+    pinecone.delete_index(INDEX_NAME)
+print(INDEX_NAME)
+pinecone.create_index(name=INDEX_NAME, 
+    dimension=model.get_sentence_embedding_dimension(), 
+    metric='cosine',
+    spec=ServerlessSpec(cloud='aws', region='us-west-2'))
+
+index = pinecone.Index(INDEX_NAME)
+print(index)
